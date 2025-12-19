d4418da45690ad2784e180d5cdafacde5b871fca
attendee <attendees@atomicmail.io>
2025-12-19 08:54:05 +0300

    add: bubble tea init

--- main.go
+++ main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+
+	// Just return `nil`, which means "no I/O right now, please." //
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Check if key press event //
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		// These keys should exit the program //
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Press 'q' or 'ctrl+c'\n\n"
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(&model{})
+
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+
+}
