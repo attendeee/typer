a4b57e2f4ac80df5e5d8d39fd2da6eb65323db3e
Alex <klawdee@mail.ru>
2026-01-19 10:59:06 +0300

    fix: deleted unused struct

--- model/model.go
+++ model/model.go
@@ -8,17 +8,3 @@ type Chapter struct {
 	Title string   `json:"title"`
 	Text  []string `json:"text"`
 }
-
-type Model struct {
-	Book      Book
-	Chapter   int
-	Text      string
-	CursorPos int
-
-	OffsetStep      int
-	Offsets         []int
-	UpperOffset     int
-	UpperOffsetIdx  int
-	BottomOffset    int
-	BottomOffsetIdx int
-}
